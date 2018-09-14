91dd726aec96910b0948f4b02aba61fcccbd39c3
Sho Hirose <sho.hirose@utexas.edu>
2018-09-13 23:46:52 -0500

    style: change variable name

--- pge383-advanced-geomechanics-fall2018/project2/geographical_to_principal.py
+++ pge383-advanced-geomechanics-fall2018/project2/geographical_to_principal.py
@@ -33,7 +33,7 @@ def calc_rotation_matrix(alpha, beta, gamma):
         [c1*s2*c3 + s1*s3, s1*s2*c3 - c1*s3, c2*c3]])
     return rot_pg
 
-def rotate_stress_tensor(sigma_g, rot):
+def rotate_stress_tensor(sigma_g, rot_pg):
     """
     Rotate stress tensor from geographical to principal stress coordinate system.
 
@@ -41,7 +41,7 @@ def rotate_stress_tensor(sigma_g, rot):
     ----------
     sigma_g : numpy.array
         stress tensor in geographical coordinate system.
-    rot : numpy.array
+    rot_pg : numpy.array
         rotation matrix from geographical to principal stress coordinate system.
 
     Returns
@@ -50,5 +50,5 @@ def rotate_stress_tensor(sigma_g, rot):
         stress tensor in principal stress coordinate system.
     """
 
-    sigma_p = np.dot(np.dot(rot, sigma_g), np.transpose(rot))
+    sigma_p = np.dot(np.dot(rot_pg, sigma_g), np.transpose(rot_pg))
     return sigma_p
