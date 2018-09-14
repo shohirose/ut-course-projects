554ea5a56f07d6906618f40cce87d02b5a4674a1
Sho Hirose <sho.hirose@utexas.edu>
2018-09-13 23:45:17 -0500

    style: add comments

--- pge383-advanced-geomechanics-fall2018/project2/geographical_to_principal.py
+++ pge383-advanced-geomechanics-fall2018/project2/geographical_to_principal.py
@@ -2,6 +2,25 @@ import numpy as np
 import math
 
 def calc_rotation_matrix(alpha, beta, gamma):
+    """
+    Calculate rotation matrix from geographical to principal stress coordinate
+    system.
+
+    Parameters
+    ----------
+    alpha : double
+        Euler angle in radians
+    beta : double
+        Euler angle in radians
+    gamma : double
+        Euler angle in radians
+
+    Returns
+    -------
+    rot_pg : numpy.array
+        rotation matrix from geographical to principal stress coordinate system
+    """
+
     c1 = math.cos(alpha)
     s1 = math.sin(alpha)
     c2 = math.cos(beta)
@@ -9,9 +28,27 @@ def calc_rotation_matrix(alpha, beta, gamma):
     c3 = math.cos(gamma)
     s3 = math.sin(gamma)
 
-    return np.array([[c1*c2, s1*c2, -s2],
+    rot_pg = np.array([[c1*c2, s1*c2, -s2],
         [c1*s2*s3 - s1*c3, s1*s2*s3 + c1*c3, c2*s3],
         [c1*s2*c3 + s1*s3, s1*s2*c3 - c1*s3, c2*c3]])
+    return rot_pg
 
-def rotate_stress_tensor(sigma, rot):
-    return np.dot(np.dot(rot, sigma), np.transpose(rot))
+def rotate_stress_tensor(sigma_g, rot):
+    """
+    Rotate stress tensor from geographical to principal stress coordinate system.
+
+    Parameters
+    ----------
+    sigma_g : numpy.array
+        stress tensor in geographical coordinate system.
+    rot : numpy.array
+        rotation matrix from geographical to principal stress coordinate system.
+
+    Returns
+    -------
+    sigma_p : numpy.array
+        stress tensor in principal stress coordinate system.
+    """
+
+    sigma_p = np.dot(np.dot(rot, sigma_g), np.transpose(rot))
+    return sigma_p
